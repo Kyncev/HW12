3158f3928a2a62c48ea5eed5098a81b443b3e8dc
Kyncev <45149572+Kyncev@users.noreply.github.com>
2023-05-14 01:19:30 +0300

    Add files via upload

---
+++ FirstRunBrowser.java
@@ -0,0 +1,22 @@
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstRunBrowser extends BaseClass {
+
+    public static void main(String[] args) {
+    driver.get("https://ithillel.ua");
+    driver.findElement(By.id("btn-consultation-hero")).click();
+    driver.findElement(By.id("input-name-consultation")).sendKeys("Vitaly");
+    driver.findElement(By.id("input-email-consultation")).sendKeys("r..gmail.com");
+    driver.findElement(By.id("input-tel-consultation")).sendKeys("+380^^^^^^^^");
+    try {
+        Thread.sleep(5000);
+
+    } catch (InterruptedException e){
+        e.printStackTrace();
+    }
+driver.quit();
+    }
+}
