7429616fc74b1f13ce77f0699512e31e8bb48674
Kyncev <45149572+Kyncev@users.noreply.github.com>
2023-05-14 01:33:39 +0300

    Update FirstRunBrowser.java

--- FirstRunBrowser.java
+++ FirstRunBrowser.java
@@ -10,7 +10,7 @@ public class FirstRunBrowser extends BaseClass {
     driver.findElement(By.id("btn-consultation-hero")).click();
     driver.findElement(By.id("input-name-consultation")).sendKeys("Vitaly");
     driver.findElement(By.id("input-email-consultation")).sendKeys("r..gmail.com");
-    driver.findElement(By.id("input-tel-consultation")).sendKeys("+380^^^^^^^^");
+    driver.findElement(By.id("input-tel-consultation")).sendKeys("662834194");
     try {
         Thread.sleep(5000);
 
